01a426fe0d8ea9d01086b53732db527524d5c306
Javohirbek <79502483+javohirbek-2002@users.noreply.github.com>
2021-03-16 17:23:31 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+
+// **************************
+
+
+
+// val=Math.floor(8.3);       
+// console.log(val);    
+
+// val=Math.PI;    
+// console.log(val);    
+// val=Math.E;    
+// console.log(val);    
+    
+// val=Math.ceil(8.9);    
+// console.log(val);    
+    
+// val=Math.abs(-2222);    
+// console.log(val);    
+
+// val=Math.pow(25, 3);    
+// console.log(val);    
+
+// val=Math.round(7.444);//7    
+// console.log(val);
+
+// val=Math.sqrt(15625);
+// console.log(val)
+
+// const num1 = 100;
+// const num2 = 200;
+
+
+// val = num1 * num2;
+// val = num1 / num2;
+// val = num1 + num2;
+// val = num1 - num2;
+// val = num1 % num2;
+// console.log(val);
+
+// praktika uchun edi bular
+
+
+// ***************************************
+// **********Task 1 * * ********************
+// ***************************************
+
+const num1 = 30;
+const num2 = 50;
+
+val = num1 * num2;
+console.log(val);
+// 30m ga ko`paytirdim 50m ni javob 1500 chiqdi yani yerni yuzasi 15 sotix 1500kv m
+
+//Ikkita ixtiyoriy nolga teng bo'lmagan a=7, b =8 sonlarning o'rta arifmetigi topilsin.
+
+//ustoz buni tushunmadim
+
